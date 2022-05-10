c0da4f1fbbe2a2d602473846c8abe70694a96b1f
Rio saputra <102573606+RioSaputra11@users.noreply.github.com>
2022-05-10 22:05:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,8 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281390658325'],
-  ['6282256115584'],
-  ['6281319944687', 'BG', true]
+  ['6282117332347'],
+  ['6282119094904', 'BG', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -30,8 +29,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot'
+global.packname = 'wa.me/682119094904'
+global.author = 'Ｒｙｏｂｏｔ－Ｍｄ V2'
 
 global.multiplier = 69 // The higher, The harder levelup
 
