512d2c7d9d3613b97a16b2675dfc7f9f288e59e7
Xynels Design <107124409+Xynelsbotz@users.noreply.github.com>
2022-07-17 10:03:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
+global.linkGC = ['https://chat.whatsapp.com/KNkpoT4oX5yFA18GlPttgl', 'https://chat.whatsapp.com/GAKysOsPAQN69Lq4d4Behk']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
-    name: 'Owner Metro Bot/Creator games-wabot',
+  '6281515453325': { // put your number here
+    name: 'Owner Xynels/Creator sayu-ogiwara',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
@@ -19,10 +19,10 @@ global.Owner = {
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '6285713964963': {
+  '6281515453325': {
     isReport: true // if true this number will send if user use command /report or /join
   },
-  '13479805233': {} // ignore if you don't want anything, like name dll
+  '13479805233': {} // if you don't want anything, like name dll
 }
 global.mods = ['13479805233'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
@@ -53,8 +53,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = 'Xynels'
+global.author = 'Sayu-Ogiwara'
 
 global.multiplier = 39 // The higher, The harder levelup
 
